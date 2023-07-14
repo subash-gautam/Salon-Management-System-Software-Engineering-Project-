93fad6e6cd6c68f4019ee5e2c3f777301cb00e55
Subash Gautam <subashgautam977@gmail.com>
2023-07-14 14:41:31 +0545

    setting data file

---
+++ styles.js
@@ -0,0 +1,24 @@
+var styles = {
+    style1 : {
+        image: 'hair1.jpeg',
+        type: "hair"
+    },
+    style2 : {
+        
+    },
+    style3 : {
+
+    },
+    style4 : {
+
+    },
+    style5 : {
+
+    },
+    style6 : {
+
+    },
+    style7 : {
+
+    }
+}
