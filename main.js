244b6cc5398a0080a5de4b0ebdc7bc212fc84f7d
Subash Gautam <subashgautam977@gmail.com>
2023-07-14 12:48:16 +0545

    css started

--- main.js
+++ main.js
@@ -1,4 +1,13 @@
 (function(){
     document.getElementById('header').innerHTML = `<h1>Salon Management System</h1>`;
-    
+    var demos = document.getElementById('demos');
+    demos.innerHTML = 'Empty demo section.';
+    var logInPannel = document.getElementById('logInPannel');
+    logInPannel.innerHTML = `<div><a href="book.html" id="booking">Book Now</a>
+    <a href="admin.html" id="barberLogin">Barber Login</a></div>`;
+
+    // Function Haru Yata
+    function initialHTMLFillUp(){
+
+    }
 })();
