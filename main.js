9886c6d0d841734fb4e3904e8169e8905a196f3d
Subash Gautam <subashgautam977@gmail.com>
2023-07-14 13:25:06 +0545

    hola jasto xa

--- main.js
+++ main.js
@@ -1,13 +1,40 @@
 (function(){
-    document.getElementById('header').innerHTML = `<h1>Salon Management System</h1>`;
-    var demos = document.getElementById('demos');
-    demos.innerHTML = 'Empty demo section.';
-    var logInPannel = document.getElementById('logInPannel');
-    logInPannel.innerHTML = `<div><a href="book.html" id="booking">Book Now</a>
-    <a href="admin.html" id="barberLogin">Barber Login</a></div>`;
+    "use strict";
+
+    { // variable declaration
+        var demos = document.getElementById('demos');
+        var logInPannel = document.getElementById('logInPannel');
+    }
+
+    {// function calls
+    initialHTMLFillUp();
+    }
 
     // Function Haru Yata
     function initialHTMLFillUp(){
+        document.getElementById('header').innerHTML = `<h1>Salon Management System</h1>`;
+        demos.innerHTML = 'Empty demo section.';
+        logInPannel.innerHTML = `<div id="logInDiv"><button id="booking">Book Now</button>
+        <button id="userLogin">Log In</button>
+        <button id="barberLogin">Barber Login</button></div>`;
 
+        document.getElementById('barberLogin').addEventListener('click', function(){
+            barberLogin();
+        });  
+        document.getElementById('userLogin').addEventListener('click', function(){
+            login();
+        });  
+        document.getElementById('booking').addEventListener('click', function(){
+            booking();
+        });  
+    }
+    function barberLogin(){
+        console.log("This is barber login function");
+    }
+    function login(){
+        console.log("This is user login function");
+    }
+    function booking(){
+        console.log("This is booking function");
     }
 })();
