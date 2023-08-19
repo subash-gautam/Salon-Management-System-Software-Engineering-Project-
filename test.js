80a97cacfe29c12a69d3c01c769d7ac45f2a4a9b
Subash Gautam <subashgautam977@gmail.com>
2023-08-19 12:04:11 +0545

    test file deleted

--- test.js
+++
@@ -1,7 +0,0 @@
-/** @format */
-
-const csvData = "John,Doe,30";
-const dataArray = csvData.split(",");
-dataArray[2] = "31"; // Update the age
-const updatedCSV = dataArray.join(",");
-console.log(updatedCSV); // Output: "John,Doe,31"
